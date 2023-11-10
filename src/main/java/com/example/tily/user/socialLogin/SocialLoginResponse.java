e524dfd2d154bf11ec4af1188a34bb5131bfd44b
sam-mae <lsslsm2@pusan.ac.kr>
2023-11-11 04:55:39 +0900

    test: til,alarm 테스트코드 작성

--- src/main/java/com/example/tily/user/socialLogin/SocialLoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.tily.user.socialLogin;
-
-public class SocialLoginResponse{
-    public record UserInfoDto(String nickname, String email){};
-
-    public record TokenDTO(String accessToken){};
-}
-
-
